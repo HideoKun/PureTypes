4203ccb693729e220bce6e2c40b3b4695c77a433
HideoKun <wo.contact@gmail.com>
2025-01-04 21:05:57 +0100

    fix: other other

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,7 +1,6 @@
-import type { GENERIC_ERROR } from "../types"
+import type { IsError_ } from "../predicates/isError"
 
 // INFO: no BOX mode here, CTA is only mode for filtering
 export type _Filter$<A, B> = A extends B ? A : never
 
-// TODO: validation?
-export type FilterError$<T> = _Filter$<T, GENERIC_ERROR>
+export type FilterError$<T> = IsError_<T> extends true ? T : never
