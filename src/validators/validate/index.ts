0d465514126a1dfb184c7e55f61c8fc22167c723
HideoKun <wo.contact@gmail.com>
2025-01-11 18:36:38 +0100

    fix: validate cleanup

--- src/validators/validate/index.ts
+++ src/validators/validate/index.ts
@@ -1,30 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type {
   AnyError,
   NeverError,
   UnknownError,
 } from "@errors"
-import type { Trace } from "@operators"
+import type { TX } from "@operators"
 import type { IsError_ } from "@predicates"
-
-export type VALIDATOR_MODES = "flat" | "chain"
 
 type Name = "_Validate$"
 
+// TODO: rename chain to either
+export type VALIDATOR_MODES = "flat" | "chain"
+
+type _ValidateAll$<
+  Data extends unknown[],
+  Acc = never,
+> = [Data] extends [[infer First, ...infer Rest]]
+  ? _ValidateAll$<
+      Rest,
+      Acc | _Validate$<First, "flat">
+    >
+  : Acc
+
 type _Validate$<
-  T$,
+  T,
   IsChainable extends VALIDATOR_MODES,
   CX extends string = "",
-> = [T$] extends [never]
-  ? NeverError<Trace<CX, Name>, T$>
-  : 0 extends 1 & T$
-    ? AnyError<Trace<CX, Name>, T$>
-    : [unknown] extends [T$]
-      ? UnknownError<Trace<CX, Name>, T$>
-      : IsChainable extends "chain"
-        ? T$
-        : IsError_<T$> extends true
-          ? T$
-          : never
+> = [T] extends [never]
+  ? NeverError<TX<CX, Name>, T>
+  : 0 extends 1 & T
+    ? AnyError<TX<CX, Name>, T>
+    : [unknown] extends [T]
+      ? UnknownError<TX<CX, Name>, T>
+      : [T] extends [any[]]
+        ? _ValidateAll$<T>
+        : IsChainable extends "chain"
+          ? T
+          : IsError_<T> extends true
+            ? T
+            : never
+
+// -----------------------------------------------------
 
 /**
  * @returns Error | never
@@ -32,10 +49,39 @@ type _Validate$<
 export type Validate$<
   T,
   CX extends string = "",
-> = _Validate$<T, "flat", CX>
+> = _Validate$<
+  //
+  T,
+  "flat",
+  TX<CX, "Validate$">
+>
 
 /**
  * @returns Error | T
  */
-export type CH_Validate<T> = _Validate$<T, "chain">
+export type EitherValidate<
+  T,
+  CX extends string = "",
+> = _Validate$<
+  T,
+  "chain",
+  TX<CX, "EitherValidate">
+>
 // TODO: fix context for validation (func index, just - to curry context)
+
+// -----------------------------------------------------
+
+type A = Validate$<never, "test">
+type B = Validate$<any, "test">
+type C = Validate$<unknown, "test">
+type E = Validate$<never[], "test">
+//   ^?
+type F = Validate$<[1, "ś", any], "test">
+//   ^?
+
+//   ^?
+// type FF = ValidateAll$<[any]>
+//   ^?
+
+type Z = IsError_<F>
+//   ^?
