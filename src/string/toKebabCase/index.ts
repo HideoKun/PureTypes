212ae46fb6f2a9ff289449999198784aef0db749
Konrad Dziekonski <jaseveen@gmail.com>
2025-01-21 20:59:16 +0100

    feat(JSDoc): add JSDoc

--- src/string/toKebabCase/index.ts
+++ src/string/toKebabCase/index.ts
@@ -1,3 +1,14 @@
+/**
+ * A ToKebabCase implementation on a type level.
+ *
+ * @template String - A string to turn into kebab case.
+ * @returns {string} - a-string-to-turn-into-kebab-case.
+ *
+ *
+ * @example
+ * type KebabCased = ToKebabCase<"MixOf_Separators And-Cases">; // Result: mix-of-separators-and-cases
+ */
+
 import type { _ToKebabCase } from "./algo"
 
 export type ToKebabCase<Str extends string> =
--- src/string/toSnakeCase/index.ts
+++ src/string/toSnakeCase/index.ts
@@ -1,3 +1,14 @@
+/**
+ * A ToSnakeCase implementation on a type level.
+ *
+ * @template String - A string to turn into snake case.
+ * @returns {string} - a_string_to_turn_into_snake_case.
+ *
+ *
+ * @example
+ * type SnakeCased = ToSnakeCase<"MixOf_Separators And-Cases">; // Result: mix_of_separators_and_cases
+ */
+
 import type { _ToSnakeCase } from "./algo"
 
 export type ToSnakeCase<Str extends string> =
